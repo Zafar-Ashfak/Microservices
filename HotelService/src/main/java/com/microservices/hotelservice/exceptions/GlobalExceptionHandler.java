a7f1e964593539a5350b4ab91559603d7e1e971e
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-07-19 17:15:33 +0530

    Add exception handler and Api response to catch error

---
+++ HotelService/src/main/java/com/microservices/hotelservice/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.microservices.hotelservice.exceptions;
+
+import com.microservices.hotelservice.payload.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex) {
+        String message =  ex.getMessage();
+        ApiResponse response = ApiResponse.builder()
+                .message(message)
+                .success(true)
+                .status(HttpStatus.NOT_FOUND)
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+}
