6e14a41252e7f292f96dd03bfa161913e1497e15
Zafar Ashfak <Zafarashfak008@gmail.com>
2026-07-21 13:15:06 +0530

    Create service-registry project to host projects on eureka server

--- ServiceRegistry/src/main/java/com/microservices/serviceregistry/ServiceRegistryApplication.java
+++ ServiceRegistry/src/main/java/com/microservices/serviceregistry/ServiceRegistryApplication.java
@@ -2,12 +2,15 @@ package com.microservices.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceRegistryApplication.class, args);
+        System.out.println("Tomcat started on port 8761");
     }
 
 }
